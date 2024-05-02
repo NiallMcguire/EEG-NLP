4b0d32cbe1dd4d9438eb79f07540f9cfc007d19d
NiallMcguire <mundanelarch@gmail.com>
2024-05-02 13:25:27 +0100

    NLP dataset with EEG

--- src/read_data.py
+++ src/read_data.py
@@ -17,18 +17,18 @@ class ReadData:
 
     def get_eeg_word_embedding(self, word, eeg_type='GD', bands=['_t1', '_t2', '_a1', '_a2', '_b1', '_b2', '_g1', '_g2']):
         EEG_frequency_features = []
-        EEG_word_level_label = word['content']
+        word_label = word['content']
         for band in bands:
             EEG_frequency_features.append(word['word_level_EEG'][eeg_type][eeg_type + band])
-        word_eeg_embedding = np.concatenate(EEG_frequency_features)
-        if len(word_eeg_embedding) != 105 * len(bands):
+        EEG_word_token = np.concatenate(EEG_frequency_features)
+        if len(EEG_word_token) != 105 * len(bands):
             print(
-                f'expect word eeg embedding dim to be {105 * len(bands)}, but got {len(word_eeg_embedding)}, return None')
-            word_eeg_embedding = None
+                f'expect word eeg embedding dim to be {105 * len(bands)}, but got {len(EEG_word_token)}, return None')
+            EEG_word_token = None
         else:
-            word_eeg_embedding = word_eeg_embedding.reshape(105, 8)
+            EEG_word_token = EEG_word_token.reshape(105, 8)
 
-        return word_eeg_embedding, EEG_word_level_label
+        return EEG_word_token, word_label
 
     def read_file(self):
         with open(self.data_path, 'rb') as f:
@@ -61,6 +61,7 @@ class ReadData:
         return Task_Dataset_List
 
     def create_train_test_datasets(self):
+
 
 
 
