e3722af69e22c2749c310d6f2d9ce23a0b926315
NiallMcguire <mundanelarch@gmail.com>
2024-05-03 16:01:25 +0100

    NLP dataset with EEG

--- src/EEG_NER/Loss.py
+++ src/EEG_NER/Loss.py
@@ -23,3 +23,26 @@ class ContrastiveLossEuclid(nn.Module):
         loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +
                                       (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
         return loss_contrastive
+
+
+class ContrastiveLossCosine(nn.Module):
+    """
+    Contrastive loss class using cosine similarity between embeddings
+
+    Args:
+        margin (float): Margin value for contrastive loss
+
+    Returns:
+        loss_contrastive (tensor): Contrastive loss value
+
+    """
+    def __init__(self, margin=1.0):
+        super(ContrastiveLossCosine, self).__init__()
+        self.margin = margin
+
+    def forward(self, output1, output2, label):
+        cosine_similarity = nn.functional.cosine_similarity(output1, output2)
+        loss_contrastive = torch.mean((1-label) * torch.pow(cosine_similarity, 2) +
+                                      (label) * torch.pow(torch.clamp(self.margin - cosine_similarity, min=0.0), 2))
+        return loss_contrastive
+
