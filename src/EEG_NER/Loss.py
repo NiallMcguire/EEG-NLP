b450eb39faa209bd2edc1c806ed29ed61d312b4c
NiallMcguire <mundanelarch@gmail.com>
2024-05-03 15:59:46 +0100

    NLP dataset with EEG

--- src/EEG_NER/Loss.py
+++ src/EEG_NER/Loss.py
@@ -15,7 +15,7 @@ class ContrastiveLossEuclid(nn.Module):
 
     """
     def __init__(self, margin=2.0):
-        super(ContrastiveLoss, self).__init__()
+        super(ContrastiveLossEuclid, self).__init__()
         self.margin = margin
 
     def forward(self, output1, output2, label):
