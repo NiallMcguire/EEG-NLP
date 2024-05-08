4114e8afbecbb8e5a2968e0aa9748c3c6ebcdd52
NiallMcguire <mundanelarch@gmail.com>
2024-05-08 15:36:43 +0100

    NLP dataset with EEG

--- scripts/EEG-NER/train.py
+++ scripts/EEG-NER/train.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     train_path = r"/users/gxb18167/Datasets/ZuCo/train_NER.pkl"
 
-    test_path = r"C:\Users\gxb18167\PycharmProjects\SIGIR_EEG_GAN\Development\Named-Entity-Classification\Data-Management\test_NER.pkl"
+    test_path = r"/users/gxb18167/Datasets/ZuCo/test_NER.pkl"
 
     EEG_path = r"C:\Users\gxb18167\PycharmProjects\EEG-To-Text\SIGIR_Development\EEG-GAN\EEG_Text_Pairs.pkl"
 
