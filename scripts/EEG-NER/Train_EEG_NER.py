d1710bc6b6dd10f3579c9b45fa57152e9acfab3c
NiallMcguire <mundanelarch@gmail.com>
2024-05-08 16:53:33 +0100

    NLP dataset with EEG

--- scripts/EEG-NER/Train_EEG_NER.py
+++ scripts/EEG-NER/Train_EEG_NER.py
@@ -21,50 +21,6 @@ from gensim.models import Word2Vec
 if __name__ == "__main__":
     log = {}
 
-
-    if torch.cuda.is_available():
-        device = torch.device("cuda")
-    else:
-        device = torch.device("cpu")
-
-    train_path = r"/users/gxb18167/Datasets/ZuCo/train_NER.pkl"
-
-    test_path = r"/users/gxb18167/Datasets/ZuCo/test_NER.pkl"
-
-    EEG_path = r"/users/gxb18167/Datasets/ZuCo/EEG_Text_Pairs.pkl"
-
-    d = data.Data()
-    util = utils.Utils()
-
-    train_NE, train_EEG_segments, train_Classes = d.NER_read_custom_files(train_path)
-    test_NE, test_EEG_segments, test_Classes = d.NER_read_custom_files(test_path)
-
-    EEG_word_tokens, EEG_word_labels = d.read_EEG_embeddings_labels(EEG_path)
-
-
-
-    # padding
-    X_train, y_train = util.NER_padding_x_y(train_EEG_segments, train_Classes)
-    X_train_numpy = np.array(X_train)
-    X_train_numpy = util.NER_reshape_data(X_train_numpy)
-    y_train_categorical = util.encode_labels(y_train)
-
-    X_test, y_test = util.NER_padding_x_y(test_EEG_segments, test_Classes)
-    X_test_numpy = np.array(X_test)
-    X_test_numpy = util.NER_reshape_data(X_test_numpy)
-    y_test_categorical = util.encode_labels(y_test)
-
-    # Convert numpy arrays to PyTorch tensors
-    x_train_tensor = torch.tensor(X_train_numpy, dtype=torch.float32)
-    y_train_tensor = torch.tensor(y_train_categorical, dtype=torch.float32)  # Assuming your labels are integers
-
-    x_test_tensor = torch.tensor(X_test_numpy, dtype=torch.float32)
-    y_test_tensor = torch.tensor(y_test_categorical, dtype=torch.float32)  # Assuming your labels are integers
-
-    # Create a custom dataset
-    train_dataset = TensorDataset(x_train_tensor, y_train_tensor)
-    test_dataset = TensorDataset(x_test_tensor, y_test_tensor)
-
     parameters = {}
 
     evaluation = True
@@ -91,6 +47,48 @@ if __name__ == "__main__":
     parameters['optimizer'] = optimizer
     criterion = 'CrossEntropyLoss'
     parameters['criterion'] = criterion
+
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+    train_path = r"/users/gxb18167/Datasets/ZuCo/train_NER.pkl"
+
+    test_path = r"/users/gxb18167/Datasets/ZuCo/test_NER.pkl"
+
+
+    d = data.Data()
+    util = utils.Utils()
+
+    train_NE, train_EEG_segments, train_Classes = d.NER_read_custom_files(train_path)
+    test_NE, test_EEG_segments, test_Classes = d.NER_read_custom_files(test_path)
+
+
+    #create word embeddings
+
+
+    # padding
+    X_train, y_train = util.NER_padding_x_y(train_EEG_segments, train_Classes)
+    X_train_numpy = np.array(X_train)
+    X_train_numpy = util.NER_reshape_data(X_train_numpy)
+    y_train_categorical = util.encode_labels(y_train)
+
+    X_test, y_test = util.NER_padding_x_y(test_EEG_segments, test_Classes)
+    X_test_numpy = np.array(X_test)
+    X_test_numpy = util.NER_reshape_data(X_test_numpy)
+    y_test_categorical = util.encode_labels(y_test)
+
+    # Convert numpy arrays to PyTorch tensors
+    x_train_tensor = torch.tensor(X_train_numpy, dtype=torch.float32)
+    y_train_tensor = torch.tensor(y_train_categorical, dtype=torch.float32)  # Assuming your labels are integers
+
+    x_test_tensor = torch.tensor(X_test_numpy, dtype=torch.float32)
+    y_test_tensor = torch.tensor(y_test_categorical, dtype=torch.float32)  # Assuming your labels are integers
+
+    # Create a custom dataset
+    train_dataset = TensorDataset(x_train_tensor, y_train_tensor)
+    test_dataset = TensorDataset(x_test_tensor, y_test_tensor)
 
     # Create the train loader
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
