6bf7869ce1dd6c3f7db30ad5b1cd8c210fbec6df
NiallMcguire <mundanelarch@gmail.com>
2024-05-10 13:00:26 +0100

    triplet loss

--- scripts/EEG-NER/Train_EEG_NER.py
+++ scripts/EEG-NER/Train_EEG_NER.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
 
     evaluation = True
     parameters['evaluation'] = evaluation
-    EEG_with_Text = True
+    EEG_with_Text = False
     parameters['EEG_with_Text'] = EEG_with_Text
     Embedding_model = 'Word2Vec'
     parameters['Embedding_model'] = Embedding_model
