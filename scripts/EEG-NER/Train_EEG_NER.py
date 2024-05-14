2f94bd442c3b152a2e9a683df80667dd1f02a8f2
NiallMcguire <mundanelarch@gmail.com>
2024-05-14 11:09:15 +0100

    added soley Text embedding classification

--- scripts/EEG-NER/Train_EEG_NER.py
+++ scripts/EEG-NER/Train_EEG_NER.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     parameters['evaluation'] = evaluation
     inputs = "Text"
     parameters['inputs'] = inputs
-    Embedding_model = 'BERT'
+    Embedding_model = 'Word2Vec' # 'Word2Vec' or 'BERT'
 
     parameters['Embedding_model'] = Embedding_model
 
