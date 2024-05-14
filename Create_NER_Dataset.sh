e3a356379a8f3019a52e8ded790d144d97a03146
NiallMcguire <mundanelarch@gmail.com>
2024-05-14 12:22:08 +0100

    removed useless file

--- Create_NER_Dataset.sh
+++ Create_NER_Dataset.sh
@@ -42,7 +42,7 @@ module load anaconda/python-3.9.7/2021.11
 
 #Modify the line below to run your program. This is an example
 #=========================================================
-python /users/gxb18167/EEG-NLP/src/read_in_test.py
+python /users/gxb18167/EEG-NLP/scripts/EEG-NER/create_NER_dataset.py
 
 # Do not change the line below
 #=========================================================
