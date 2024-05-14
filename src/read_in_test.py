37fad54ddd3ede117572745cc1884a6fc73453a0
NiallMcguire <mundanelarch@gmail.com>
2024-05-14 11:53:27 +0100

    updated read in file from ZuCo

--- src/read_in_test.py
+++ src/read_in_test.py
@@ -6,6 +6,13 @@ if __name__ == "__main__":
     task_name = "task1, task2, taskNRv2"
 
     data = data.Data()
-    data.create_custom_dataset(task_name)
+    EEG_word_level_embeddings, EEG_word_level_labels = data.create_custom_dataset(task_name)
+
+    EEG_word_level_sentences, EEG_sentence_embeddings = data.NER_get_sentences_EEG(EEG_word_level_labels,
+                                                                          EEG_word_level_embeddings)
+
+    list_of_words = data.NER_combine_sentences(EEG_word_level_sentences)
+
+
 
     print("Data created successfully!")
