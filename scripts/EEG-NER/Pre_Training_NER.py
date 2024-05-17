a378dcf0d0c3cbe68bb5e14d9218bfc5da7b3690
NiallMcguire <mundanelarch@gmail.com>
2024-05-17 09:48:39 +0100

    joint embedding

--- scripts/EEG-NER/Pre_Training_NER.py
+++ scripts/EEG-NER/Pre_Training_NER.py
@@ -4,7 +4,7 @@ import torch.optim as optim
 from src import Networks
 from src import data
 from src import utils
-
+import numpy as np
 
 
 
@@ -15,4 +15,16 @@ if __name__ == "main":
     util = utils.Utils()
 
     train_NE, train_EEG_segments, train_Classes = d.NER_read_custom_files(train_path)
+
+    vector_size = 768
+
+    ner_bert = utils.NER_BERT()
+
+    train_NE_embedded = ner_bert.get_embeddings(train_NE)
+
+    train_NE_expanded = util.NER_expanded_NER_list(train_EEG_segments, train_NE_embedded, vector_size)
+
+    train_NE_expanded = np.array(train_NE_expanded)
+
+    train_NE_padded_tensor = torch.tensor(train_NE_expanded, dtype=torch.float32)
 
