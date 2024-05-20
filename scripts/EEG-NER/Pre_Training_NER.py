fecba29e2462d5d50e374f2d16255f1d7f59533b
NiallMcguire <mundanelarch@gmail.com>
2024-05-20 14:27:32 +0100

    added contrastive loss to pre-training

--- scripts/EEG-NER/Pre_Training_NER.py
+++ scripts/EEG-NER/Pre_Training_NER.py
@@ -6,6 +6,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from sklearn.model_selection import train_test_split
+import torch.nn.functional as F
 
 
 from src import Networks
@@ -13,6 +14,36 @@ from src import data
 from src import utils
 import numpy as np
 
+
+from torch.utils.data import Dataset, DataLoader
+
+class EEGToBERTContrastiveDataset(Dataset):
+    def __init__(self, eeg_data, bert_data, labels):
+        self.eeg_data = eeg_data
+        self.bert_data = bert_data
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        eeg_vector = self.eeg_data[idx]
+        bert_vector = self.bert_data[idx]
+        label = self.labels[idx]
+        return eeg_vector, bert_vector, label
+
+
+
+class ContrastiveLoss(nn.Module):
+    def __init__(self, margin=1.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, output1, output2, label):
+        euclidean_distance = F.pairwise_distance(output1, output2)
+        loss_contrastive = torch.mean((1 - label) * torch.pow(euclidean_distance, 2) +
+                                      label * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+        return loss_contrastive
 
 
 if __name__ == "__main__":
@@ -64,3 +95,8 @@ if __name__ == "__main__":
     eeg_train, eeg_test, bert_train, bert_test, labels_train, labels_test = train_test_split(eeg_pairs, bert_pairs, labels, test_size=test_size, random_state=42)
 
 
+    train_dataset = EEGToBERTContrastiveDataset(eeg_train, bert_train, labels_train)
+    test_dataset = EEGToBERTContrastiveDataset(eeg_test, bert_test, labels_test)
+
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True)
