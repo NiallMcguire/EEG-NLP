a877b2a56423f02660f6d115a7d6543abf1e7a72
NiallMcguire <mundanelarch@gmail.com>
2024-05-24 11:57:50 +0100

    added validation and early stopping

--- src/Loss.py
+++ src/Loss.py
@@ -15,14 +15,16 @@ class ContrastiveLossEuclid(nn.Module):
         loss_contrastive (tensor): Contrastive loss value
 
     """
-    def __init__(self, margin=2.0):
+    def __init__(self, margin=1.0):
         super(ContrastiveLossEuclid, self).__init__()
         self.margin = margin
 
     def forward(self, output1, output2, label):
-        euclidean_distance = nn.functional.pairwise_distance(output1, output2)
-        loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +
-                                      (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+        euclidean_distance = F.pairwise_distance(output1, output2)
+        # Reshape label tensor to match the shape of euclidean_distance tensor
+        label = label.unsqueeze(1).expand_as(euclidean_distance)
+        loss_contrastive = torch.mean((1 - label) * torch.pow(euclidean_distance, 2) +
+                                      label * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
         return loss_contrastive
 
 
