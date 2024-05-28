296559f9cb192c6853e904311e4f20fee65b23be
NiallMcguire <mundanelarch@gmail.com>
2024-05-28 22:39:32 +0100

    added projection layer

--- scripts/EEG-NER/Grid_Pre_Training_NER.py
+++ scripts/EEG-NER/Grid_Pre_Training_NER.py
@@ -248,7 +248,7 @@ if __name__ == "__main__":
         'optimizer': ["Adam"],
         'learning_rate': [0.0001],
         'Embedding_model': ['BERT'],
-        'model_name': ['EEGToBERTModel_v5']
+        'model_name': ['EEGToBERTModel_v4']
     }
 
     train_path = r"/users/gxb18167/EEG-NLP/NER.pkl"
