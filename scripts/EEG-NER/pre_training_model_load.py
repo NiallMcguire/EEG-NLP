d805ea67f5128c3a03a0ef62fb01aa26b95f2819
NiallMcguire <mundanelarch@gmail.com>
2024-05-30 13:49:08 +0100

    model to device

--- scripts/EEG-NER/pre_training_model_load.py
+++ scripts/EEG-NER/pre_training_model_load.py
@@ -12,10 +12,17 @@ if __name__ == "__main__":
 
     config_paths = "/users/gxb18167/configs/"
 
+    models = ['EEGToBERTModel_v4', 'EEGToBERTModel_v3']
+
 
     #list all files in directory
     for file in os.listdir(config_paths):
         #check if file is a .json file and contains NER
-        if file.endswith(".json") and "NER" in file:
+        if file.endswith(".json") and "NER_Pre_Training" in file:
             parameter_dictionary = util.load_json(config_paths + file)
+            for model in models:
+                if model in parameter_dictionary['model_name']:
+                    print(f"Loading model: {model}")
+
+
 
