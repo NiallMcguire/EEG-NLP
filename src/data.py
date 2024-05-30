2aba2c7f56478dbe4f6cc8c7a577cc37b7de0886
NiallMcguire <mundanelarch@gmail.com>
2024-05-30 16:40:54 +0100

    model to device

--- src/data.py
+++ src/data.py
@@ -338,6 +338,8 @@ class Data:
                 aligned_EEG = torch.cat((aligned_EEG, aligned_EEG_outputs), dim=0)
                 aligned_y = torch.cat((aligned_y, batch_y), dim=0)
 
+            pre_train_model.train()
+            pre_train_model.detach()
             tensor_dataset = TensorDataset(aligned_EEG, aligned_y)
             return tensor_dataset
 
