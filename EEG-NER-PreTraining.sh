54caa89c29dc7ea6955c8aba83914537302fe3d4
NiallMcguire <mundanelarch@gmail.com>
2024-06-03 17:01:17 +0100

    cleaning

--- EEG-NER-PreTraining.sh
+++ EEG-NER-PreTraining.sh
@@ -42,7 +42,7 @@ module load anaconda/python-3.9.7/2021.11
 
 #Modify the line below to run your program. This is an example
 #=========================================================
-python /users/gxb18167/EEG-NLP/scripts/EEG-NER/Pre_Training_NER.py
+python /users/gxb18167/EEG-NLP/scripts/EEG-Contrastive/Pre_Training.py
 
 # Do not change the line below
 #=========================================================
