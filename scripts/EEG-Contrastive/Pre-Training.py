36e5fb37ebc1914a3b41e60cf303b5e785df2263
NiallMcguire <mundanelarch@gmail.com>
2024-06-03 16:05:12 +0100

    cleaning

---
+++ scripts/EEG-Contrastive/Pre-Training.py
@@ -0,0 +1,29 @@
+import sys
+
+sys.path.append('/users/gxb18167/EEG-NLP')
+
+
+import torch
+import torch.nn as nn
+from sklearn.model_selection import train_test_split
+import torch.nn.functional as F
+from src import data
+from src import utils
+from src import Networks
+from src import Loss
+import numpy as np
+from random import sample
+import datetime
+from torch.utils.data import Dataset, DataLoader
+
+
+if __name__ == "__main__":
+    train_path = r"C:\Users\gxb18167\PycharmProjects\EEG-NLP\NER.pkl"
+
+
+    d = data.Data()
+    util = utils.Utils()
+    Loss = Loss
+    Networks = Networks
+
+    train_NE, train_EEG_segments, train_Classes = d.NER_read_custom_files(train_path)
