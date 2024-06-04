41923c462e311cd9e56b85289d327c6783de6693
NiallMcguire <mundanelarch@gmail.com>
2024-06-04 12:52:32 +0100

    added LSTM v3 siamese model

--- scripts/EEG-Contrastive/Pre-Training.py
+++ scripts/EEG-Contrastive/Pre-Training.py
@@ -148,7 +148,7 @@ if __name__ == "__main__":
 
     max_positive_pairs = 20000
     max_negative_pairs = 20000
-    contrastive_learning_setting = 'EEGtoEEG' #EEGtoBERT, EEGtoEEG
+    contrastive_learning_setting = 'EEGtoBERT' #EEGtoBERT, EEGtoEEG
     vector_size = 768
     parameters['vector_size'] = vector_size
     ner_bert = utils.NER_BERT()
