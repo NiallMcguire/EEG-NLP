ccdbc269bed78e2372453b7e8c450cc31becc222
NiallMcguire <mundanelarch@gmail.com>
2024-06-04 10:52:19 +0100

    cleaning

--- src/Networks.py
+++ src/Networks.py
@@ -133,4 +133,21 @@ class EEGToBERTModel_v4(nn.Module):
 
 import torch.nn as nn
 
+class SiameseNetwork_v1(nn.Module):
+    def __init__(self, input_dim):
+        super(SiameseNetwork_v1, self).__init__()
+        self.fc1 = nn.Linear(input_dim, 512)
+        self.fc2 = nn.Linear(512, 128)
+        self.fc3 = nn.Linear(128, 64)
 
+    def forward_once(self, x):
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    def forward(self, input1, input2):
+        output1 = self.forward_once(input1)
+        output2 = self.forward_once(input2)
+        return output1, output2
