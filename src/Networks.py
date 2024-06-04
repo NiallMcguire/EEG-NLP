a1f094f17d0396de3f092f187867110d115962c2
NiallMcguire <mundanelarch@gmail.com>
2024-06-04 11:35:26 +0100

    added LSTM v3 siamese model

--- src/Networks.py
+++ src/Networks.py
@@ -161,31 +161,22 @@ class SiameseNetwork_v2(nn.Module):
 
         # First convolutional layer
         self.conv1 = nn.Conv1d(in_channels=7, out_channels=32, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm1d(32)
-        self.relu1 = nn.ReLU()
 
         # Second convolutional layer
         self.conv2 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
-        self.bn2 = nn.BatchNorm1d(64)
-        self.relu2 = nn.ReLU()
 
         # Max pooling layer
         self.max_pool = nn.MaxPool1d(kernel_size=2, stride=2)
 
         # Fully connected layers
         self.fc1 = nn.Linear(64 * 420, 128)
-        self.relu3 = nn.ReLU()
         self.fc2 = nn.Linear(128, 64)  # Adjust the output size based on your task
 
     def forward_once(self, x):
         # Convolutional layers
         x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu1(x)
 
         x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu2(x)
 
         # Max pooling
         x = self.max_pool(x)
@@ -195,7 +186,6 @@ class SiameseNetwork_v2(nn.Module):
 
         # Fully connected layers
         x = self.fc1(x)
-        x = self.relu3(x)
         x = self.fc2(x)
 
         return x
