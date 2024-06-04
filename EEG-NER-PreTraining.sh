9c0b50b474eb61298b2f989ac15beace997a3926
NiallMcguire <mundanelarch@gmail.com>
2024-06-04 10:29:04 +0100

    cleaning

--- EEG-NER-PreTraining.sh
+++ EEG-NER-PreTraining.sh
@@ -17,7 +17,7 @@
 #SBATCH --account=moshfeghi-pmwc
 #
 # Specify (hard) runtime (HH:MM:SS)
-#SBATCH --time=10:00:00
+#SBATCH --time=1:00:00
 #SBATCH --mail-user=niall.mcguire@strath.ac.uk
 #SBATCH --mail-type=END
 # Job name
