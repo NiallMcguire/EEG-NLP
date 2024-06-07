dfa8b5f9cb5e001871e3800cf72a1c8cb88346be
NiallMcguire <mundanelarch@gmail.com>
2024-06-07 14:01:51 +0100

    added BERT LSTM input

--- scripts/EEG-Contrastive/Loop_Through_Models.py
+++ scripts/EEG-Contrastive/Loop_Through_Models.py
@@ -1,4 +1,4 @@
-
+import json
 import os
 
 
@@ -7,8 +7,30 @@ import os
 if __name__ == "__main__":
     config_save_path = "/users/gxb18167/configs/"
 
+    target_parameters = {}
+
+    target_parameters['contrastive_learning_setting'] = ['EEGtoEEG']
+    target_parameters['model_name'] = ['SiameseNetwork_v1', 'SiameseNetwork_v2', 'SiameseNetwork_v3']
+
 
     # print files in the directory
     print("Files in the directory:")
     for file in os.listdir(config_save_path):
-        print(file)
+        # if file contains EEG_NER_Pre_Training and json file
+        if 'EEG_NER_Pre_Training' in file and '.json' in file:
+            #load json file
+            with open('data.json', 'r') as file:
+                data = json.load(file)
+
+                # check if the file contains the target parameters
+                for key in target_parameters.keys():
+                    if key in data.keys():
+                        for value in target_parameters[key]:
+                            if value in data[key]:
+                                # print model_save_path
+                                print(data['model_save_path'])
+
+
+
+
+
