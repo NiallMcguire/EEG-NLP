73ee2d39acdde0f65226329db4fac5d70e548982
NiallMcguire <mundanelarch@gmail.com>
2024-06-07 14:10:42 +0100

    added BERT LSTM input

--- scripts/EEG-Contrastive/Loop_Through_Models.py
+++ scripts/EEG-Contrastive/Loop_Through_Models.py
@@ -3,6 +3,31 @@ import os
 
 
 
+def fine_target_models(config_path, args):
+    # print files in the directory
+    pre_training_model_paths = []
+
+
+    for file in os.listdir(config_path):
+        # if file contains EEG_NER_Pre_Training and json file
+        if 'EEG_NER_Pre_Training' in file and '.json' in file:
+            #load json file
+            with open(config_path + file, 'r') as doc:
+                data = json.load(doc)
+                # check if the file contains the target parameters
+                for key in args.keys():
+                    if key in data.keys():
+                        for value in args[key]:
+                            if value in data[key]:
+                                # print model_save_path
+                                print("Model matching target parameters saved @", data['model_save_path'])
+                                pre_training_model_paths.append(data['model_save_path'])
+
+    return pre_training_model_paths
+
+
+
+
 
 if __name__ == "__main__":
     config_save_path = "/users/gxb18167/configs/"
